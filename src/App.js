be249728761ac7715a421d8f112f554ebf6d9899
sudenurcevik <sudenur867@gmail.com>
2023-05-20 00:45:20 +0300

    new fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import AppRoutes from "./routes/router";
-import Dashboard from "./pages/Dashboard/Dashboard";
-// import Header from "./layouts/Header";
-// import Footer from "./layouts/Footer";
 
 function App() {
   return (
