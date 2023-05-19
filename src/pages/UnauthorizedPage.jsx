bd801b4bc34223481b7310396f474b5ad692d628
sudenurcevik <sudenur867@gmail.com>
2023-05-19 18:03:21 +0300

    not found page is fixed

---
+++ src/pages/UnauthorizedPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UnauthorizedPage() {
+  return <>Bu sayfaya erişim hakkınız yoktur.</>;
+}
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -9,15 +9,14 @@ import { getViewAuthorizationForAll } from "../helpers/AuthorizationHelper";
 import SessionHelper from "../helpers/SessionHelper";
 import Navbar from "../components/Navbar/navbar";
 
-
 // lazy loading components for better performance
 const LandingPage = lazy(() => import("../pages/LandingPage"));
 const Login = lazy(() => import("../pages/Login"));
 const Dashboard = lazy(() => import("../pages/Dashboard"));
-const AnnouncementPage = lazy(() => import("../pages/AnnouncementPage/AnnouncementPage"));
-const AnnouncementDetailsPage = lazy(() => import("../pages/AnnouncementPage/AnnouncementDetailsPage"));
+const AnnouncementPage = lazy(() =>
+  import("../pages/AnnouncementPage/AnnouncementPage")
+);
 const NotFound = lazy(() => import("../components/NotFound"));
-
 
 const auth = [
   {
@@ -27,14 +26,10 @@ const auth = [
   },
 ];
 
-const publicRoutes = [
+const privateRoutes = [
   {
     path: "/announcements",
     component: AnnouncementPage,
-  },
-  {
-    path: "/announcement/:id",
-    component: AnnouncementDetailsPage,
   },
   {
     path: "/dashboard",
@@ -89,29 +84,31 @@ export default function AppRoutes() {
     init();
   }, [init, user]);
 
+  const ProtectedRoutes = () => (
+    <Switch>
+      {privateRoutes.map((route, index) => (
+        <Route key={index} path={route.path} exact={route.exact}>
+          <Navbar drawerList={drawerList} component={<route.component />} />
+        </Route>
+      ))}
+      {/* Add the NotFound route here */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route path="/" exact component={LandingPage}></Route>
+          <Route path="/" exact component={LandingPage} />
           {auth.map((route, index) => (
             <Route key={index} path={route.path} exact={route.exact}>
               <route.component update={update} setUpdate={setUpdate} />
             </Route>
-
           ))}
           <PrivateRoute path="/">
-            {publicRoutes.map((route, index) => (
-              <Route key={index} path={route.path} exact={route.exact}>
-                <Navbar
-                  component={<route.component />}
-                  drawerList={drawerList}
-                />
-              </Route>
-            ))}
+            <ProtectedRoutes />
           </PrivateRoute>
-          {/* TODO: 404 page not work! */}
-          <Route path="*" component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
