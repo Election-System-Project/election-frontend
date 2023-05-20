e323432370fca85d0d6144e8408ac05fd9e2ef8b
Burak Keçeci <bburakkececi@gmail.com>
2023-05-20 13:43:29 +0300

    AnnouncementCreatePage added.

--- src/pages/AnnouncementPage/AnnouncementCreatePage.jsx
+++ src/pages/AnnouncementPage/AnnouncementCreatePage.jsx
@@ -1,12 +1,19 @@
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, Divider, Paper, Typography } from '@mui/material'
 import { Container } from '@mui/system'
 import React from 'react'
+import AnnouncementForm from './components/AnnouncementForm'
 
 export default function AnnouncementCreatePage() {
   return (
     <CssBaseline>
         <Container maxWidth="lg">
-            <h1>AnnouncementCreatePage</h1>
+          <Paper sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Typography variant="h4" component="h2" gutterBottom>
+              Create Announcement
+            </Typography>
+            <Divider />
+            <AnnouncementForm />
+          </Paper>
         </Container>
     </CssBaseline>
   )
--- src/pages/AnnouncementPage/components/AnnouncementForm.jsx
+++ src/pages/AnnouncementPage/components/AnnouncementForm.jsx
@@ -1,7 +1,197 @@
-import React from 'react'
+import * as React from 'react';
+import FormControl from '@mui/base/FormControl';
+import { makeStyles } from '@mui/styles';
+import { Alert, Box, Button, FormHelperText, MenuItem, TextField } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import SendIcon from '@mui/icons-material/Send';
+
+const useStyles = makeStyles((theme) => ({
+  inputControl: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  selectList: {
+    minWidth: 120,
+    maxHeight: 40,
+  },
+  content: {
+    resize: 'none',
+    width: '60%',
+    padding: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+  },
+  btn: {
+    padding: theme.spacing(2),
+    width: '20%',
+    alignSelf: 'flex-end',
+  },
+}));
+
+const announcementTypes = [
+  {
+    value: 'general',
+    label: 'General Announcement',
+  },
+  {
+    value: 'result',
+    label: 'Result Announcement',
+  },
+]
 
 export default function AnnouncementForm() {
+
+  const classes = useStyles();
+  const action = '/announcements/create';
+
+  const history = useHistory();
+  
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [announcementType, setAnnouncementType] = React.useState('');
+  const [error, setError] = React.useState(false);
+
+  const onAnnouncementTypeChange = (event) => {
+    setAnnouncementType(event.target.value);
+  };
+
+  const onTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onTextChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (title === '' || description === '' || announcementType === '') {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    const data = {
+      title: title,
+      content: description,
+      announcement_type: announcementType
+    };
+    console.log(data);
+
+    fetch(action, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+      redirect: 'follow',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      }
+      )
+      .then((result) => {
+        console.log(result);
+        history.push('/announcements');
+      }
+      )
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      }
+      );
+
+  };
+
+
   return (
-    <div>AnnouncementForm</div>
+    <form action={action} method='POST' onSubmit={onSubmit} className={classes.form}>
+      {
+      error && 
+      <Alert severity="error" style={{margin: '10px 10px', width: '40%',}}>
+        Please fill all the fields.
+      </Alert>
+      }
+      <FormControl required className={classes.inputControl}>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { width: "25ch" }
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-select-currency"
+            select
+            required
+            label="Announcement Type"
+            defaultValue={announcementType}
+            onChange={onAnnouncementTypeChange}
+          >
+            {announcementTypes.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <FormHelperText>Required</FormHelperText>
+        </Box>
+      </FormControl>
+      <FormControl className={classes.inputControl} required>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { width: "25ch" }
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            required
+            id="title"
+            label="Title"
+            variant="outlined"
+            defaultValue={title}
+            onChange={onTitleChange}
+          />
+          <FormHelperText>Required</FormHelperText>
+        </Box>
+      </FormControl>
+      <FormControl className={classes.inputControl} required>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { width: "50ch" }
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="description"
+            label="Description"
+            multiline
+            required
+            rows={5}
+            variant="outlined"
+            defaultValue={description}
+            onChange={onTextChange}
+          />
+          <FormHelperText>Required</FormHelperText>
+        </Box>
+      </FormControl>
+      <Button type="submit" variant="contained" color="error" endIcon={<SendIcon />} className={classes.btn}>Send</Button>
+    </form>
   )
 }
