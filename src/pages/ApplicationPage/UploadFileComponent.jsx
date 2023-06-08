6e8d4673bd440abca5ceb9b2b525b2dd18690cfb
sudenurcevik <sudenur867@gmail.com>
2023-06-09 02:38:04 +0300

    application page

--- src/pages/ApplicationPage/UploadFileComponent.jsx
+++ src/pages/ApplicationPage/UploadFileComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import useStyles from "./ApplicationPage.styles";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, CircularProgress } from "@mui/material";
 import { Delete } from "@material-ui/icons";
 
 function UploadFileComponent({
@@ -10,9 +10,9 @@ function UploadFileComponent({
   handleDeleteFile,
   files,
   disabled,
+  loading,
 }) {
   const classes = useStyles();
-  console.log(files);
   return (
     <div className={classes.applicationCard}>
       <div className={classes.text}>
@@ -47,32 +47,37 @@ function UploadFileComponent({
           Choose a File
         </Button>
       </label>
-      {files && files?.length !== 0 && (
-        <Typography
-          variant="body1"
-          className={classes.fileName}
-          sx={{
-            display: "inline-block",
-            border: "dashed",
-            marginLeft: "2rem",
-            padding: "1rem",
-            maxWidth: "400px",
-            height: "auto",
-          }}
-        >
-          {files?.name}
-          <Button
-            onClick={() => handleDeleteFile(index)}
+      {loading ? (
+        <CircularProgress style={{ color: "#999" }} />
+      ) : (
+        files &&
+        files?.length !== 0 && (
+          <Typography
+            variant="body1"
+            className={classes.fileName}
             sx={{
               display: "inline-block",
-              color: "white",
-              justifyContent: "right",
+              border: "dashed",
+              marginLeft: "2rem",
+              padding: "1rem",
+              maxWidth: "400px",
+              height: "auto",
             }}
-            disabled={disabled}
           >
-            <Delete />
-          </Button>
-        </Typography>
+            {files?.name}
+            <Button
+              onClick={() => handleDeleteFile(index)}
+              sx={{
+                display: "inline-block",
+                color: "white",
+                justifyContent: "right",
+              }}
+              disabled={disabled}
+            >
+              <Delete />
+            </Button>
+          </Typography>
+        )
       )}
     </div>
   );
